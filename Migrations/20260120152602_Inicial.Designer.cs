4927fd8d09c28ccf1f59edb8e31f708642c02784
Igor Fernandes <54910774+1rg0@users.noreply.github.com>
2026-01-20 12:32:59 -0300

    feat: implementação de ícones
    implementação de ícones

--- Migrations/20260120152602_Inicial.Designer.cs
+++ Migrations/20260120152602_Inicial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Jogos_Backlogger.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260117124856_Inicial")]
+    [Migration("20260120152602_Inicial")]
     partial class Inicial
     {
         /// <inheritdoc />
